e4e5039e5e44b82faad8c255da03c15ca03e7d7f
Arun David <arundavid.info@gmail.com>
2013-12-21 11:52:53 +0530

    Update

--- application/config/tweet.php
+++ application/config/tweet.php
@@ -1,4 +1,4 @@
 <?php
 
-	$config['tweet_consumer_key'] = "eW3VI12k3sItUvMQsYUWeQ";
-	$config['tweet_consumer_secret'] = "BbtpIkMpoVWPtjALQXCxEyO4BK6i5Ohn5w0m51juEg";
+	$config['tweet_consumer_key'] = "";
+	$config['tweet_consumer_secret'] = "";
